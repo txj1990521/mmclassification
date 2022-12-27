198d3d65c230caf0c66404f123941d6d852d747c
JACK <39322210+txj1990521@users.noreply.github.com>
2022-12-27 17:23:17 +0800

    12271723

--- side_ai/pipelines/init_pipelines.py
+++ side_ai/pipelines/init_pipelines.py
@@ -742,3 +742,37 @@ class CopyErrorPath:
     def __repr__(self):
         repr_str = self.__class__.__name__
         return repr_str
+
+
+@PIPELINES.register_module()
+class GenerateMmclsAnn:
+    def __call__(self, results, *args, **kwargs):
+        data_dir = results['data_dir']
+        img_type = results['img_type']
+        data_dir = str(Path(data_dir)) + '/'
+        classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        class2id = dict(zip(classes, range(len(classes))))
+        data_dir = str(Path(data_dir)) + '/'
+        dir_types = ['train', 'val', 'test']
+        sub_dirs = os.listdir(data_dir)
+        ann_dir = data_dir + 'meta/'
+        if not os.path.exists(ann_dir):
+            os.makedirs(ann_dir)
+        for sd in sub_dirs:
+            if sd not in dir_types:
+                continue
+            annotations = []
+            target_dir = data_dir + sd + '/'
+            for d in os.listdir(target_dir):
+                class_id = str(class2id[d])
+                images = glob(target_dir + d + '/*' + img_type)
+                for img in images:
+                    img = d + '/' + os.path.basename(img)
+                    annotations.append(img + ' ' + class_id + '\n')
+            annotations[-1] = annotations[-1].strip()
+            with open(ann_dir + sd + '.txt', 'w') as f:
+                f.writelines(annotations)
+
+    def __repr__(self):
+        repr_str = self.__class__.__name__
+        return repr_str
