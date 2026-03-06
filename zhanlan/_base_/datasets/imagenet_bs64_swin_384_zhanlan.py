9c771e804b8850d0a1a32eb4d75c08da41b38284
JACK <39322210+txj1990521@users.noreply.github.com>
2026-03-06 16:49:14 +0800

    Create imagenet_bs64_swin_384_zhanlan.py
    data文件

---
+++ zhanlan/_base_/datasets/imagenet_bs64_swin_384_zhanlan.py
@@ -0,0 +1,58 @@
+data_root = r'D:/zhanlan/Classify/split_data/'
+metainfo = dict(classes=['格子', '条纹', '正常'])
+# dataset settings
+dataset_type = 'ImageNet'
+data_preprocessor = dict(
+    num_classes=3,
+    # RGB format normalization parameters
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    # convert image from BGR to RGB
+    to_rgb=True,
+)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='RandomResizedCrop',
+        scale=384,
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='PackInputs'),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=384, backend='pillow', interpolation='bicubic'),
+    dict(type='PackInputs'),
+]
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=1,
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        split='train',
+        metainfo=metainfo,
+        pipeline=train_pipeline),
+    sampler=dict(type='DefaultSampler', shuffle=True),
+)
+
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=1,
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        split='val',
+        metainfo=metainfo,
+        pipeline=test_pipeline),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+)
+val_evaluator = dict(type='Accuracy', topk=(1, ))
+
+# If you want standard test, please manually configure the test dataset
+test_dataloader = val_dataloader
+test_evaluator = val_evaluator
